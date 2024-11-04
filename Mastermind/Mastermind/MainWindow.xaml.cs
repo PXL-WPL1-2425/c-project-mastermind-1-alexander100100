65dc1b788fda35069497103599a7c560fbc53767
Alexander Mahrhofer <12401690@student.pxl.be>
2024-11-04 14:37:49 +0100

    Mastermind-02

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -16,9 +16,23 @@ namespace Mastermind
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<string> _colors = new List<string> { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
+        private readonly string[] _generatedCode = new string[4];
+
         public MainWindow()
+
         {
             InitializeComponent();
+            GenerateRandomCode();
+        }
+        private void GenerateRandomCode()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                _generatedCode[i] = _colors[random.Next(_colors.Count)];
+            }
+            this.Title = $"Mastermind - Code: {string.Join(", ", _generatedCode)}";
         }
     }
 }
