c73043cbd3d6a35e01c23f62d9191ae458c39166
Alexander Mahrhofer <12401690@student.pxl.be>
2024-11-17 22:14:34 +0100

    MasterMind-05

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -18,12 +18,14 @@ namespace Mastermind
     public partial class MainWindow : Window
     {
         private readonly List<string> colors = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        private List<string> generatedCode = new List<string>();
 
 
         public MainWindow()
         {
             InitializeComponent();
             FillComboBoxes();
+            GenerateCode();
         }
 
         private void FillComboBoxes()
@@ -32,6 +34,17 @@ namespace Mastermind
             ComboBox2.ItemsSource = colors;
             ComboBox3.ItemsSource = colors;
             ComboBox4.ItemsSource = colors;
+        }
+        private void GenerateCode()
+        {
+            Random random = new Random();
+            generatedCode.Clear();
+            for (int i = 0; i < 4; i++)
+            {
+                int index = random.Next(colors.Count);
+                generatedCode.Add(colors[index]);
+            }
+            this.Title = $"MasterMind ({string.Join(",", generatedCode)})";
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -67,7 +80,60 @@ namespace Mastermind
 
         private void Validatebtn_Click(object sender, RoutedEventArgs e)
         {
+            List<string> userCode = new List<string>
+            {
+                ComboBox1.SelectedItem?.ToString(),
+                ComboBox2.SelectedItem?.ToString(),
+                ComboBox3.SelectedItem?.ToString(),
+                ComboBox4.SelectedItem?.ToString()
+            };
 
+            if (userCode.Contains(null))
+            {
+                MessageBox.Show("Please select a color for each slot before validating.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (userCode[i] == generatedCode[i])
+                {
+                    ShowBorder(i, Colors.DarkRed);
+                }
+                else if (generatedCode.Contains(userCode[i]))
+                {
+                    ShowBorder(i, Colors.Wheat);
+                }
+                else
+                {
+                    ShowBorder(i, Colors.Transparent);
+                }
+            }
+        }
+
+        private void ShowBorder(int index, Color color)
+        {
+            Brush borderColor = new SolidColorBrush(color);
+
+            switch (index)
+            {
+                case 0:
+                    Label1.BorderBrush = borderColor;
+                    Label1.BorderThickness = new Thickness(3);
+                    break;
+                case 1:
+                    Label2.BorderBrush = borderColor;
+                    Label2.BorderThickness = new Thickness(3);
+                    break;
+                case 2:
+                    Label3.BorderBrush = borderColor;
+                    Label3.BorderThickness = new Thickness(3);
+                    break;
+                case 3:
+                    Label4.BorderBrush = borderColor;
+                    Label4.BorderThickness = new Thickness(3);
+                    break;
+            }
         }
     }
 }
